c23d5eaee0ac64cd135e6f2e803bd7406906b550
confusedd <56777997+milliways-png@users.noreply.github.com>
2024-03-30 18:28:58 -0600

    load diabetes dataset

--- proyecto_1.py
+++ proyecto_1.py
@@ -6,3 +6,6 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+
+## Data loading
+dataframe = pandas.read_csv("data/diabetes.csv")
