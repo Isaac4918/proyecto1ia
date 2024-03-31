70e49653c70cc1e0de42407f7826f7ccde939b3d
confusedd <56777997+milliways-png@users.noreply.github.com>
2024-03-30 19:05:16 -0600

    add interquartile_range function

--- proyecto_1.py
+++ proyecto_1.py
@@ -9,3 +9,9 @@ from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 
 ## Data loading
 dataframe = pandas.read_csv("data/diabetes.csv")
+## Data analysis
+### Outliers and invalid values
+def interquartile_range(variable, dataframe):
+    Q1 = dataframe[variable].quantile(0.25)
+    Q3 = dataframe[variable].quantile(0.75)
+    return (Q1, Q3, Q3 - Q1)
