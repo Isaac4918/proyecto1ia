7bbedc571147000a9235426b2d18fa3e7e1667fb
confusedd <56777997+milliways-png@users.noreply.github.com>
2024-03-31 11:46:01 -0600

    add more scatterplots

--- proyecto_1.py
+++ proyecto_1.py
@@ -47,12 +47,32 @@ dataframe.corr(method='pearson', min_periods=1, numeric_only=False)
 dataframe.corr(method='spearman', min_periods=1, numeric_only=False)
 # Data Visualization
 blood_pressure = dataframe.BloodPressure
+age = dataframe.Age
 glucose = dataframe.Glucose
 outcome = dataframe.Outcome
-age = dataframe.Age
+bmi = dataframe.BMI
+insulin = dataframe.Insulin
 
 scatter_plot = plt.scatter(glucose, age, c=outcome)
 plt.colorbar(scatter_plot)
 plt.xlabel("glucose")
 plt.ylabel("age")
+plt.show()
+
+scatter_plot2 = plt.scatter(blood_pressure, glucose, c=outcome)
+plt.colorbar(scatter_plot2)
+plt.xlabel("blood pressure")
+plt.ylabel("glucose")
+plt.show()
+
+scatter_plot3 = plt.scatter(insulin, glucose, c=outcome)
+plt.colorbar(scatter_plot3)
+plt.xlabel("insulin")
+plt.ylabel("glucose")
+plt.show()
+
+scatter_plot4 = plt.scatter(bmi, glucose, c=outcome)
+plt.colorbar(scatter_plot4)
+plt.xlabel("bmi")
+plt.ylabel("glucose")
 plt.show()
