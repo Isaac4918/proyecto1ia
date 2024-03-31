5993bc3906c02bb5a3a4bc868e75b8be6d469481
confusedd <56777997+milliways-png@users.noreply.github.com>
2024-03-30 18:23:11 -0600

    add imports

---
+++ proyecto_1.py
@@ -0,0 +1,8 @@
+## Imports
+import pandas
+import matplotlib.pyplot as plt
+from math import sqrt
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
