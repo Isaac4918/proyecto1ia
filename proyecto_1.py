a7502f4250256955a992d04ec093df3c57341c4a
confusedd <56777997+milliways-png@users.noreply.github.com>
2024-03-30 19:12:53 -0600

    add remove_outliers function

--- proyecto_1.py
+++ proyecto_1.py
@@ -15,3 +15,17 @@ def interquartile_range(variable, dataframe):
     Q1 = dataframe[variable].quantile(0.25)
     Q3 = dataframe[variable].quantile(0.75)
     return (Q1, Q3, Q3 - Q1)
+
+def remove_outliers (dataframe):
+    threshold = 1.5
+    for i in range(len(dataframe.columns)-1):
+        Q1 = interquartile_range(dataframe.columns[i], dataframe)[0]
+        Q3 = interquartile_range(dataframe.columns[i], dataframe)[1]
+        IQR = interquartile_range(dataframe.columns[i], dataframe)[2]
+        outliers = dataframe[(dataframe[dataframe.columns[i]] < Q1 - threshold * IQR) | (dataframe[dataframe.columns[i]] > Q3 + threshold * IQR)]
+        dataframe = dataframe.drop(outliers.index)
+    return dataframe
+
+dataframe = dataframe.dropna() # drop empty values
+dataframe = remove_outliers(dataframe) # drop outliers
+
