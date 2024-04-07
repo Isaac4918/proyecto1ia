1c7695993775d32027d7ca0cec580ec6fbe70e82
confusedd <56777997+milliways-png@users.noreply.github.com>
2024-04-06 19:48:43 -0600

    deal with outliers and nan values

--- proyecto_1_titanic.py
+++ proyecto_1_titanic.py
@@ -16,3 +16,27 @@ dataframe = pandas.get_dummies(dataframe, columns=['Pclass', 'Sex','Embarked'], 
 ### Dropping columns
 # Drop unnecessary columns
 dataframe.drop(['Cabin','PassengerId','Name','Ticket'], axis='columns', inplace=True)
+### Outliers and invalid values
+def interquartile_range(variable, dataframe):
+    Q1 = dataframe[variable].quantile(0.25)
+    Q3 = dataframe[variable].quantile(0.75)
+    return (Q1, Q3, Q3 - Q1)
+
+
+def remove_outliers(dataframe: pandas.DataFrame, dataframe_columns):
+    threshold = 1.5
+
+    for i in range(len(dataframe_columns)):
+        (Q1, Q3, IQR) = interquartile_range(dataframe_columns[i], dataframe)
+        low_outliers = dataframe[dataframe[dataframe_columns[i]] < Q1 - threshold * IQR]
+        dataframe = dataframe.drop(low_outliers.index)
+        high_outliers = dataframe[dataframe[dataframe_columns[i]] > Q3 + threshold * IQR]
+        dataframe = dataframe.drop(high_outliers.index)
+    return dataframe
+
+def remove_nan_from_column(dataframe, column_name):
+    mean = dataframe[column_name].mean()
+    dataframe[column_name].replace(np.NaN, mean, inplace=True) 
+
+remove_nan_from_column(dataframe, 'Age')
+dataframe = remove_outliers(dataframe, ['Age', 'Fare']) # drop outliers
