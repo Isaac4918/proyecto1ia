e44153dd01de9ddd124585775744b28230f5145b
confusedd <56777997+milliways-png@users.noreply.github.com>
2024-04-06 19:46:36 -0600

    drop unnecessary columns

--- proyecto_1_titanic.py
+++ proyecto_1_titanic.py
@@ -13,3 +13,6 @@ dataframe = pandas.read_csv("data/titanic.csv")
 ### One-hot encoding 
 # One-hot encoding categorical data for 'Pclass' and 'Sex' columns
 dataframe = pandas.get_dummies(dataframe, columns=['Pclass', 'Sex','Embarked'], dtype=int)
+### Dropping columns
+# Drop unnecessary columns
+dataframe.drop(['Cabin','PassengerId','Name','Ticket'], axis='columns', inplace=True)
